f885f9fe89f46341c59d562f8b3bcc9391eca252
Estebaan93 <94663767+Estebaan93@users.noreply.github.com>
2023-11-23 10:23:29 -0300

    MoviendoElmentos

---
+++ Practicas/PARCIAL EDA/Ej4/Ej4.cpp
@@ -0,0 +1,15 @@
+/*Ejercicio 1.- Definir una estructura Pila cuya capacidad máxima sea de 100 elementos y que pueda almacenar elementos Personas (Nombre, Apellido, Edad).
+a.- Realizar una función que reciba una pila de personas y una edad. La función debe retornar una matriz (de 2 dimensiones) con los nombres, apellidos
+y edades de todas las personas que superen esa edad.
+Nota: La Pila deberá quedar en su estado original al finalizar la función. Tener en cuenta que la función solo puede utilizar las funciones públicas de la Pila.
+b.- Realizar una función que reciba la matriz generada por el ejercicio (a) y retorne un vector (1 Dimensión) con los nombres, apellidos y edades concatenados
+formando el mensaje “La persona apellido, nombre tiene
+una edad de edad años”.
+c.- Realizar una función que reciba la pila de personas y retorne una cola de personas ordenadas en base a su edad.
+d.- El programa principal debe:
+* Cargar 5 Personas en una variable Pila de Personas (No se debe pedir los datos al usuario, tienen que ser harcodeados en el programa).
+* Imprimir el contenido de la pila
+* Imprimir el contenido de la matriz devuelta por la función del ejercicio (a)
+* Imprimir el contenido del vector devuelto por la función del ejercicio (b)
+* Imprimir el contenido de la cola devuelta por la función del ejercicio (c)
+* Imprimir el contenido de la pila*/
